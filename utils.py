85d705f33eb2f385126ab793a4808b2610dd672b
Haron-Benfaida <110578609+Haron-Benfaida@users.noreply.github.com>
2024-06-27 10:38:02 +0100

    Implement Data to Box Correspondence

--- utils.py
+++ utils.py
@@ -1,10 +1,9 @@
 class Utils:
-    
     @staticmethod
-    def str_to_pairs(str):
+    def str_to_pairs(string):
         counter_key_val, counter_item = 0,0
-        pairs= [[''] * 2 for _ in range(5)]
-        for char in str:
+        pairs = [[''] * 2 for _ in range(5)]
+        for char in string:
             if char == ';':
                 counter_item += 1
                 counter_key_val = 0
@@ -17,17 +16,49 @@ class Utils:
         return pairs
 
     @staticmethod
-    def remove_str_artefacts(str):
-        str = str.replace('\n', "")
-        str = str.replace("'", "")
-        str = str.replace(" ", "")
-        str = str.replace(",", "")
-        str = str.replace(".", "")
-        return str[str.index('{') + 1 : str.index('}')]
+    def remove_str_artefacts(string):
+        string = string.replace('\n', "")
+        string = string.replace("'", "")
+        string = string.replace(" ", "")
+        string = string.replace(",", "")
+        string = string.replace(".", "")
+        string = string.replace("/", "")
+        string = string.replace("\\", "")
+        return string[string.index('{') + 1: string.index('}')]
     
     @staticmethod
-    def query_to_dict(str):
-        str = Utils.remove_str_artefacts(str)
-        pairs = Utils.str_to_pairs(str)
-        dict = {key : val for key, val in pairs}
-        return dict
+    def query_to_dict(string):
+        string = Utils.remove_str_artefacts(string)
+        pairs = Utils.str_to_pairs(string)
+        return {key: val for key, val in pairs}
+
+    @staticmethod
+    def minimum_mutations(source, typed, limit):
+        if typed == source:
+            return 0
+        if limit < 0:
+            return 0
+        if source == "":
+            return len(typed)
+        if typed == "":
+            return len(source)
+        if typed[0] == source[0]:
+            return Utils.minimum_mutations(typed[1:], source[1:], limit)
+        else:
+            add = 1 + Utils.minimum_mutations(source[0] + typed, source, limit - 1)
+            remove = 1 + Utils.minimum_mutations(typed[1:], source, limit - 1)
+            substitute = 1 + Utils.minimum_mutations(source[0] + typed[1:], source, limit - 1)
+            return min(add, remove, substitute)
+
+    @staticmethod
+    def closest_finder(string, arr):
+        lengths = []
+        for candidate in arr:
+            if not candidate:
+                continue
+            lengths.append(Utils.minimum_mutations(string, candidate, 999))
+        return lengths.index(min(lengths))
+
+
+
+
